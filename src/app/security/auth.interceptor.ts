ac48881281a8aef37a8530008f3fc43d27f8058e
Shallow-RB <ryan.bhola2601@gmail.com>
2022-04-01 23:51:39 +0200

    auth test

--- src/app/security/auth.interceptor.ts
+++ src/app/security/auth.interceptor.ts
@@ -9,23 +9,23 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const access_token = localStorage.getItem("access_token")
-
-    if ((!request.url.endsWith("/login") || !request.url.endsWith("/register") || !access_token)) {
-      request = request.clone({
-        headers: request.headers
-          .set("Authorization", "Bearer " + access_token)
-          .set("Access-Control-Allow-Origin", "*")
-      }
-      );
-    }
-
-    request = request.clone({
-      headers: request.headers
-        .set("Accept", "*/*")
-        .set("Access-Control-Allow-Origin", "*")
-        .set("Allow-Origin", "*")
-    })
+    // const access_token = localStorage.getItem("access_token")
+    //
+    // if ((!request.url.endsWith("/login") || !request.url.endsWith("/register") || !access_token)) {
+    //   request = request.clone({
+    //     headers: request.headers
+    //       .set("Authorization", "Bearer " + access_token)
+    //       .set("Access-Control-Allow-Origin", "*")
+    //   }
+    //   );
+    // }
+    //
+    // request = request.clone({
+    //   headers: request.headers
+    //     .set("Accept", "*/*")
+    //     .set("Access-Control-Allow-Origin", "*")
+    //     .set("Allow-Origin", "*")
+    // })
 
     return next.handle(request);
   }
